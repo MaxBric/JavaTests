1da3fc1a2930935df9c1c0e83ce9692068c55577
Maxence Bricault <maxence.bricault@gmail.com>
2017-01-20 09:31:08 +0100

    Update architecture

--- src/main/java/fr/imie/IConnector.java
+++ src/main/java/fr/imie/IConnector.java
@@ -1,6 +1,12 @@
 package fr.imie;
 
+import fr.imie.Exceptions.InvalidVolumeException;
+import fr.imie.enums.Messages;
+
+/**
+ * Created by Nicolas MORICET on 19/01/2017.
+ */
 public interface IConnector {
-    void placeOrder(Long volume, String product);
-    void closeOrder(Long volume, String product);
+    Messages placeOrder(Long volume, String product);
+    Messages closeOrder(Long volume, String product) throws InvalidVolumeException;
 }
--- src/main/java/fr/imie/connectors/LMAX.java
+++ src/main/java/fr/imie/connectors/LMAX.java
@@ -1,13 +1,17 @@
 package fr.imie.connectors;
 
 import fr.imie.IConnector;
+import fr.imie.enums.Messages;
 
+/**
+ * Created by Nicolas MORICET on 19/01/2017.
+ */
 public class LMAX implements IConnector {
-    public void placeOrder(Long volume, String product) {
-
+    public Messages placeOrder(Long volume, String product) {
+        return null;
     }
 
-    public void closeOrder(Long volume, String product) {
-
+    public Messages closeOrder(Long volume, String product) {
+        return null;
     }
 }
